8177b4d11fcbe83c7fb06657820719737f4f0664
Rehan Theiveehathasan <theivers@mcmaster.ca>
2018-01-27 20:05:38 -0500

    Made context menu have only 1 option

--- Chrome_Extension/context.js
+++ Chrome_Extension/context.js
@@ -4,15 +4,9 @@ function genericOnClick(info, tab) {
   console.log("item " + info.menuItemId + " was clicked");
   console.log("info: " + JSON.stringify(info));
   console.log("tab: " + JSON.stringify(tab));
-  
 }
 
 var context = "selection"
 var title = "Inform Wolfram"
-var title2 = "Inform Wiki"
 var id = chrome.contextMenus.create({"title": title, "contexts":[context],
                                        "onclick": genericOnClick});
-var id = chrome.contextMenus.create({"title": title2, "contexts":[context],
-                                       "onclick": genericOnClick});
-
-
